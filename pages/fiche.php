ef9496270dd91400e426b471ed4865742e9c865f
Holynola <angeridolf.dev@gmail.com>
2023-01-01 01:15:35 +0000

    Création de la fiche CV-AV

--- pages/fiche.php
+++ pages/fiche.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Informations détaillées | CV-AV Bouaké</title>
+    <title>Fiche CV-AV | CV-AV Bouaké</title>
     <link rel="stylesheet" href="../font-awesome-4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="../public/css/all.css">
     <link rel="stylesheet" href="../public/css/design.css">
@@ -14,7 +14,7 @@
 
     <div class="design">
         <div class="bloc-design">
-            <?php require_once '../public/php/lst.html' ; ?>
+            <?php require_once '../public/php/fch.html' ; ?>
             <?php require_once '../public/php/aside.html' ; ?>
         </div>
     </div>
