f70b3f983e1bfcafca76aa7b73e1cbe704b4bfac
Holynola <angeridolf.dev@gmail.com>
2023-01-10 15:41:55 +0000

    Création de la page Login

---
+++ pages/felix.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Création de compte | CV-AV Bouaké</title>
+    <link rel="stylesheet" href="../font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../public/css/all.css">
+    <link rel="stylesheet" href="../public/css/design.css">
+    <link rel="stylesheet" href="../public/css/felix.css">
+</head>
+<body>
+    <?php require_once '../public/php/header.html' ; ?>
+
+    <div class="design">
+        <div class="bloc-design">
+            <section>
+                <h4>Félicitations !!!</h4>
+                <p>Votre compte a été ajouté avec succès. Veuillez contacter le Bureau Diocésain pour l'activation de votre compte.</p>
+                <a href="contacts.php">Contacter le BD</a>
+                <a href="work.php">Revenir sur les Effectifs</a>
+            </section>
+            
+            <?php require_once '../public/php/aside.html' ; ?>
+        </div>
+    </div>
+
+    <?php require_once '../public/php/footer.html' ; ?>
+</body>
+</html>
